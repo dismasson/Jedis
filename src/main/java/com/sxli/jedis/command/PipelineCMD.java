9d7851f9750b995398f505d2a3e402eb19744a39
李顺翔 <lishunxiang@boshtc.com>
2018-10-11 11:59:24 +0800

    pipeline提高Redis性能，在大批量的请求的时候可以合并到一个pipeline提高系统性能，此次测试结果查询100000条数据不使用pipeline耗时11秒，使用pipeline耗时568毫秒，性能提升显著

---
+++ src/main/java/com/sxli/jedis/command/PipelineCMD.java
@@ -0,0 +1,59 @@
+package com.sxli.jedis.command;
+
+import com.sxli.jedis.pool.RedisPool;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
+public class PipelineCMD {
+
+    private Jedis jedis;
+
+    @Before
+    public void init() {
+        System.out.println("Junit测试初始化Jedis");
+        jedis = RedisPool.getJedis();
+
+    }
+
+    @After
+    public void destory() {
+        System.out.println("\nJunit测试销毁Jedis");
+        RedisPool.jedisClose(jedis);
+    }
+
+    /**
+     * 不使用pipeline
+     */
+    @Test
+    public void notUsePipeline() {
+        //开始时间
+        Long beginDate = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            String key = "key" + i;
+            jedis.get(key);
+        }
+        //结束时间
+        Long endDate = System.currentTimeMillis();
+        System.out.println("不使用pipeline，耗时:" + (endDate - beginDate) + "毫秒");
+    }
+
+    /**
+     * 使用pipeline提高性能
+     */
+    @Test
+    public void usePipeline() {
+        //开始时间
+        Long beginDate = System.currentTimeMillis();
+        Pipeline pipeline = jedis.pipelined();
+        for (int i = 0; i < 100000; i++) {
+            String key = "key" + i;
+            pipeline.get(key);
+        }
+        //结束时间
+        Long endDate = System.currentTimeMillis();
+        System.out.println("使用pipeline，耗时:" + (endDate - beginDate) + "毫秒");
+    }
+}
